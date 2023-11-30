2d6146b3398994c61e43dd11c4c8a30f67a846eb
Jack Caldwell-Nichols <jackcaldwell-nichols@Jacks-MacBook-Pro.local>
2023-11-30 12:49:18 +0100

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+const getPeoplePromise = (fetch) => {
+  return fetch("https://swapi.py4e.com/api/people")
+    .then((res) => res.json())
+    .then((data) => {
+      return {
+        count: data.count,
+        results: data.results,
+      };
+    });
+};
+
+const getPeople = async (fetch) => {
+  const res = await fetch("https://swapi.py4e.com/api/people");
+  const data = await res.json();
+
+  return {
+    count: data.count,
+    results: data.results,
+  };
+};
+
+module.exports = {
+  getPeople,
+  getPeoplePromise,
+};
