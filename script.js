36f20b4f5fda856632f2185ba59d32a455ea2832
Jack Caldwell-Nichols <jackcaldwell-nichols@Jacks-MacBook-Pro.local>
2023-12-11 12:56:50 +0100

    more testing with Rick and Morty API

--- script.js
+++ script.js
@@ -21,7 +21,31 @@ const getPeople = async (fetch) => {
   };
 };
 
+const getRickAndMortyCharacter = (fetch) => {
+  return fetch("https://rickandmortyapi.com/api/character/2")
+    .then((res) => res.json())
+    .then((data) => {
+      return {
+        name: data.name,
+        status: data.status,
+      };
+    });
+};
+
+const getPeopleFromRickAndMorty = async (fetch) => {
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await res.json();
+
+  return {
+    count: data.info.count,
+    pages: data.info.pages,
+    results: data.results,
+  };
+};
+
 module.exports = {
   getPeople,
   getPeoplePromise,
+  getRickAndMortyCharacter,
+  getPeopleFromRickAndMorty,
 };
--- script.test.js
+++ script.test.js
@@ -1,18 +1,58 @@
 const fetch = require("node-fetch");
 const swapi = require("./script.js");
 
-it("calls swapi to get people", (done) => {
-  expect.assertions(1);
-  swapi.getPeople(fetch).then((data) => {
-    expect(data.count).toEqual(87);
+// it("calls swapi to get people", (done) => {
+//   expect.assertions(1);
+//   swapi.getPeople(fetch).then((data) => {
+//     expect(data.count).toEqual(87);
+//     done();
+//   });
+// });
+
+// it("calls swapi to get people", (done) => {
+//   expect.assertions(2);
+//   swapi.getPeoplePromise(fetch).then((data) => {
+//     expect(data.count).toEqual(87);
+//     expect(data.results.length).toBeGreaterThan(5);
+//     done();
+//   });
+// });
+
+// it("getPeople returns counts and results", () => {
+//   const mockFetch = jest.fn().mockReturnValue(
+//     Promise.resolve({
+//       json: () =>
+//         Promise.resolve({
+//           count: 87,
+//           results: [0, 1, 2, 3, 4, 5],
+//         }),
+//     })
+//   );
+//   expect.assertions(4);
+//   return swapi.getPeoplePromise(mockFetch).then((data) => {
+//     expect(mockFetch.mock.calls.length).toBe(1);
+//     expect(mockFetch).toHaveBeenCalledWith("https://swapi.py4e.com/api/people");
+//     expect(data.count).toEqual(87);
+//     expect(data.results.length).toBeGreaterThan(5);
+//   });
+// });
+
+it("calls r&m to get person", (done) => {
+  expect.assertions(2);
+  swapi.getRickAndMortyCharacter(fetch).then((data) => {
+    expect(data.status).toEqual("Alive");
+    expect(data.name).toContain("Morty");
+    expect(data.length);
     done();
   });
 });
-it("calls swapi to get people", (done) => {
-  expect.assertions(2);
-  swapi.getPeoplePromise(fetch).then((data) => {
-    expect(data.count).toEqual(87);
-    expect(data.results.length).toBeGreaterThan(5);
+
+it("calls rick and morty to get people", (done) => {
+  expect.assertions(3);
+  swapi.getPeopleFromRickAndMorty(fetch).then((data) => {
+    expect(data.count).toEqual(826);
+    expect(data.results.length).toBeGreaterThan(10);
+    expect(data.pages).toBe(42);
     done();
   });
 });
